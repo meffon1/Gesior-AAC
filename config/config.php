5ee4aa49653f2c4063056ab3613bec6fdcdfe926
Marco Oliveira <marcomoa@hotmail.com>
2017-09-04 16:41:30 -0300

    New Webshop
    Modifications made by: João Leonardo Pereira

--- config/config.php
+++ config/config.php
@@ -1,25 +1,23 @@
 <?PHP
 // ----Config Shop----
 $outfits_list = array();
-
-$loyalty_title = array(
-50 => 'Scout',
-100 => 'Sentinel', 
-200 => 'Steward', 
-400 => 'Warden', 
-1000 => 'Squire', 
-2000 => 'Warrior', 
-3000 => 'Keeper', 
-4000 => 'Guardian', 
-5000 => 'Sage');
-
+$loyalty_title = array(50 => 'Scout', 100 => 'Sentinel', 200 => 'Steward', 400 => 'Warden', 1000 => 'Squire', 2000 => 'Warrior', 3000 => 'Keeper', 4000 => 'Guardian', 5000 => 'Sage');
 $config['shop']['newitemdays'] = 1; #days to item be new in shop
 
-# Pagseguro and PayPal configs
-$config['pagseguro']['testing'] = false; # if pagseguro testing mode
+# Pagseguro configs
+$config['pagseguro']['testing'] = false;
 $config['pagseguro']['tokentest'] = "";
-$config['pagseguro']['email'] = ""; #pagseguro e-mail
-$config['pagseguro']['token'] = ""; #pagseguro token
+$config['pagseguro']['email'] = '';
+$config['pagseguro']['apitoken'] = '';
+
+# Bank transfer data
+$config['banktransfer']['bank'] = "Caixa Econômica";
+$config['banktransfer']['agency'] = "";
+$config['banktransfer']['account'] = "";
+$config['banktransfer']['name'] = "";
+$config['banktransfer']['operation'] = 003; // 0 = no operation
+
+# PayPal configs
 $config['paypal']['email'] = "";
 
 # Social Networks
@@ -60,6 +58,11 @@ $towns_list = array(
 14 => 'Venore', 
 15 => 'Yalahar',
 );
+# this is important if you want to use ajax check in your create account
+$config['site']['sqlHost'] = "localhost";
+$config['site']['sqlUser'] = "root";
+$config['site']['sqlPass'] = "";
+$config['site']['sqlBD'] = "";
 
 # Create Account Options
 $config['site']['one_email'] = false;
@@ -76,19 +79,22 @@ $config['site']['max_players_per_account'] = 7;
 
 # Emails Config
 $config['site']['send_emails'] = true;
-$config['site']['mail_address'] = "@gmail.com";
+$config['site']['mail_address'] = "";
+$config['site']['mail_senderName'] = "";
 $config['site']['smtp_enabled'] = true;
-$config['site']['smtp_host'] = "ssl://smtp.gmail.com";
+$config['site']['smtp_host'] = "";
 $config['site']['smtp_port'] = 465;
 $config['site']['smtp_auth'] = true;
-$config['site']['smtp_user'] = "@gmail.com";
+$config['site']['smtp_user'] = "";
 $config['site']['smtp_pass'] = "";
+$config['site']['smtp_secure'] = true;
 
 # PAGE: accountmanagement.php
 $config['site']['send_mail_when_change_password'] = true;
 $config['site']['send_mail_when_generate_reckey'] = true;
 $config['site']['email_time_change'] = 7;
 $config['site']['daystodelete'] = 7;
+$config['site']['flash_client_enabled'] = false;
 
 # PAGE: guilds.php
 $config['site']['guild_need_level'] = 1;
@@ -116,6 +122,21 @@ $config['site']['accounts_hidden'] = array(1);
 
 # PAGE: lostaccount.php
 $config['site']['email_lai_sec_interval'] = 180;
+
+#  Pagseguro offers
+// ValorR$ => Qtd_coins
+$config['pagseguro']['offers'] = array(
+    500=>75,
+    800=>125,
+    1500=>250,
+    2800=>500,
+    4900=>1000
+);
+// Nome do produto
+$config['pagseguro']['produtoNome'] = 'Tibia Coins';
+
+$config['pagseguro']['urlRedirect'] =  'https://127.0.0.1/?subtopic=buypoints&action=realizado';
+$config['pagseguro']['urlNotification'] = 'http://127.0.0.1/retpagseguro.php';
 
 # Layout Config
 $config['site']['layout'] = 'tibiarl';
