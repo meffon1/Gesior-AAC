7a1ce95bd3eafcbbcc055ce881e859cd7d69ef97
Marco Oliveira <marcomoa@hotmail.com>
2017-09-06 15:24:29 -0300

    Pagina duplicada

--- layouts/tibiarl/layout.php
+++ layouts/tibiarl/layout.php
@@ -414,14 +414,6 @@ if(!defined('INITIALIZED'))
 									<div class="RightChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
   									</div>
 						</a>
-						<a href='?subtopic=team'>
-								<div id="submenu_team" class="Submenuitem" onmouseover="MouseOverSubmenuItem(this)" onmouseout="MouseOutSubmenuItem(this)">
-									<div class="LeftChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
-									<div id="ActiveSubmenuItemIcon_team" class="ActiveSubmenuItemIcon" style="background-image:url(<?php echo $layout_name; ?>/images/global/menu/icon-activesubmenu.gif);"></div>
-									<div id="ActiveSubmenuItemLabel_team" class="SubmenuitemLabel">Support Game</div>
-									<div class="RightChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
-  									</div>
-						</a>
 						<a href='?subtopic=tibiarules'>
 								<div id="submenu_tibiarules" class="Submenuitem" onmouseover="MouseOverSubmenuItem(this)" onmouseout="MouseOutSubmenuItem(this)">
 									<div class="LeftChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
